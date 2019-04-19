ae988621f0216644a2409c4722b1e4c188a05810
CandyFet <32927062+CandyFet@users.noreply.github.com>
2019-04-19 10:43:22 +0300

    refactoring

--- lesson2/months_hash.rb
+++ lesson2/months_hash.rb
@@ -1,17 +1,17 @@
 month_hash = {
-january: 31,
-february: 28,
-march: 31,
-april: 30,
-may: 31,
-june: 30,
-july: 31,
-august: 31,
-september: 30,
-october: 31,
-november: 30,
-december: 31
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  december: 31
 }
 month_hash.each do |month, day|
-  puts ("In #{month} #{day} days") if value == 30
+  puts("In #{month} #{day} days") if value == 30
 end
