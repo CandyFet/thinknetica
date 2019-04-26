92f294e162bd10eba09765e244ba79a72a76f346
root <inorect.a.z@gmail.com>
2019-04-26 11:18:25 +0300

    For review

--- lesson4/main.rb
+++ lesson4/main.rb
@@ -42,15 +42,23 @@ class Main
     puts 'Чтобы просмотреть список станций и список поездов на станции нажмите 8'
     puts 'Чтобы выйти из программы нажмите 0'
     puts '=' * 88
-    @user_menu_answer = gets.to_i unless @user_menu_answer.between?(0, 8)
+    @user_menu_answer = gets.to_i
+    unless @user_menu_answer.between?(0, 8)
+    end
   end
 
   def create_new_station
+    puts 'Введите название станции'
+    name = gets.chomp.capitalize!
     @stations.push(Station.new(name))
+    show_user_menu
   end
 
   def create_new_train
+    puts 'Введите номер поезда'
+    number = gets.to_i
     @trains.push(Train.new(number))
+    show_user_menu
   end
 
   def routes_menu_description
@@ -59,8 +67,10 @@ class Main
     puts 'Для добавления станции в маршрут нажмите 2'
     puts 'Для удаления станции из маршрута нажмите 3'
     puts 'Для возврата в главное меню нажмите 0'
-    puts 88 * '-'
-    @routes_menu_answer = gets.to_i unless @routes_menu_answer.between?(0, 3)
+    puts '-' * 88
+    @routes_menu_answer = gets.to_i
+    unless @routes_menu_answer.between?(0, 3)
+    end
   end
 
   def routes_menu
@@ -74,20 +84,26 @@ class Main
   end
 
   def create_new_route
-    @routes.push(Route.new(choose_a_station, choose_a_station))
+    puts 'Введите начальную и конечную станцию'
+    start_station = choose_a_station
+    end_station = choose_a_station
+    @routes.push(Route.new(start_station, end_station))
+    show_user_menu
   end
 
   def add_a_new_sation_to_route
     @routes[choose_a_route].add_station(choose_a_station)
+    show_user_menu
   end
 
   def delete_station_from_route
     @routes[choose_a_route].delete_station(choose_a_station)
+    show_user_menu
   end
 
   def routes_list
     puts 'Список маршрутов :'
-    @routes.map.with_index { |item, index| print("#{index + 1}:#{item}, ") }
+    @routes.map.with_index { |item, index| print("#{index + 1}:#{item.stations}, ") }
   end
 
   def choose_a_route
@@ -95,15 +111,17 @@ class Main
     puts 'Введите номер маршрута, с которым нужно провести операцию'
     choosed_route = gets.to_i - 1
     @routes[choosed_route]
+    show_user_menu
   end
 
   def set_route_to_train
     @trains[choose_a_train].route(choose_a_route)
+    show_user_menu
   end
 
   def trains_list
     puts 'Список всех поездов :'
-    @trains.map.with_index { |item, index| print("#{index + 1}:#{item}, ") }
+    @trains.map.with_index { |item, index| print("#{index + 1}:#{item.number}, ") }
   end
 
   def choose_a_train
@@ -111,11 +129,12 @@ class Main
     puts 'Введите номер поезда, с которым нужно провести операцию'
     choosed_train = gets.to_i - 1
     @trains[choosed_train]
+    show_user_menu
   end
 
   def station_list
     puts 'Список всех станций :'
-    @stations.map.with_index { |item, index| print("#{index + 1}:#{item}, ") }
+    @stations.map.with_index { |item, index| print("#{index + 1}:#{item.name}, ") }
   end
 
   def choose_a_station
@@ -132,6 +151,7 @@ class Main
   def delete_a_carriage
     train = choose_a_train
     train.delete_carriage(train.carriages.last)
+    show_user_menu
   end
 
   def choose_type_of_carriage
@@ -140,9 +160,19 @@ class Main
     puts 'Введите 2 для выбора пассажирского вагона'
     choosed_carriage = gets.to_i - 1
     CARRIAGE_TYPES[choosed_carriage] unless choosed_carriage.between?(1, 2)
+    show_user_menu
   end
 
   def move_train_menu
 
   end
+
+  def train_menu_discriprion
+    puts 'Введите 1 чтобы переместить поезд на следующую станцию'
+    puts 'Введите 2 чтобы переместить поезд на предыдущую станцию'
+    puts 'Введите 0 чтобы перейти в главное меню'
+
+  end
 end
+
+Main.new.show_user_menu
--- lesson4/station.rb
+++ lesson4/station.rb
@@ -1,6 +1,5 @@
 class Station
-  attr_reader :trains
-
+  attr_reader :name, :trains
   def initialize(name)
     @name = name
     @trains = []
